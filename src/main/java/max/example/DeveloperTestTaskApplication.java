9285d74bfb33df41f28a865b22ebdddaaeac8ebd
mzhivotovskiy <mzhivotovskiy@diasoft.ru>
2023-09-15 17:10:43 +0300

    init commit

---
+++ src/main/java/max/example/DeveloperTestTaskApplication.java
@@ -0,0 +1,13 @@
+package max.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperTestTaskApplication.class, args);
+    }
+
+}
